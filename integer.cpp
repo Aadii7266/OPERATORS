d285f27090cf49f84f28e610b84addb590aae8bd
Aadii7266 <adityaparihar7266@gmail.com>
2024-10-09 09:07:46 +0530

    Add files via upload

---
+++ integer.cpp
@@ -0,0 +1,29 @@
+
+//AADITYA PARIHAR
+//23070123001
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a;
+    cout<<"Enter  number:\n";
+    cin>>a;
+    if(a>=0)
+        {
+            cout<<"Positive number";
+        }
+    else
+        {
+            cout<<"Negative number";
+        }
+}
+
+/*OUTPUT:
+
+Enter a number: 5
+Positive number.
+
+Enter a number: -6
+Negative number.*/
